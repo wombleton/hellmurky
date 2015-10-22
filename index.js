fbed8aaa8addaa843682f6fe5f514d22d186177c
Rowan Crawford <wombleton@gmail.com>
2015-10-22 22:25:34 +1300

    Refactor routes & add in tile API.

--- index.js
+++ index.js
@@ -14,8 +14,6 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-
 var app = express();
 
 // view engine setup
@@ -30,7 +28,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
+require('./routes')(app);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
---
+++ routes/tile/tile.model.js
@@ -0,0 +1,35 @@
+'use strict';
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var TileSchema = new Schema({
+  createdAt: {
+    default: Date.now,
+    type: Date
+  },
+  url: String,
+  x: Number,
+  y: Number,
+  size: Number,
+  votes: [
+    {
+      bee: Boolean,
+      parasite: Boolean,
+      bad: Boolean,
+      referer: String,
+      timestamp: {
+        default: Date.now,
+        type: Date
+      }
+    }
+  ]
+});
+
+TileSchema.pre('save', function (next) {
+  this.updatedAt = new Date();
+
+  next();
+});
+
+module.exports = mongoose.model('Tile', TileSchema);
